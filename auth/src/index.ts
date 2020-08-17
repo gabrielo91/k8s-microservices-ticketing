a3ae5039120bb3b911f9c8fc2204f9777aa339f1
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-08-16 23:32:26 -0500

    Error handling implemented

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import "express-async-errors";
 import { healthCheckRouter } from "./routes/health-check";
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
@@ -15,6 +18,12 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`running on port ${PORT}!!!`);
