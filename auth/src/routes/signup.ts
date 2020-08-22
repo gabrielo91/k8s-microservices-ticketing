313d8bf34998540f54f9516f636881801532a950
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-08-22 00:33:06 -0500

    user creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { User } from "../models/user";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
@@ -14,15 +15,26 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be provided"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
 
-    throw new DatabaseConnectionError();
-    res.send(req.body);
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log("Email in use");
+      return res.send({});
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
