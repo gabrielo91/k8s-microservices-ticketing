3aca97c8fc3232ac017492ba915f2929d19ab65e
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-09-17 16:14:08 -0500

    signin tests added

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,50 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("it fails when email that does not exist is supplied", async () => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "somePass",
+    })
+    .expect(400);
+});
+
+it("fails when an incorrect password is supplied", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "somePass",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "someOtherPass",
+    })
+    .expect(400);
+});
+
+it("responds with a cookie when given valid credentials", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "somePass",
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "somePass",
+    })
+    .expect(200);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
