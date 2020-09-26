f5942db12936f8776835c9263d99fa83a40d3a15
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-09-26 00:34:12 -0500

    Error handler added

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,16 +4,19 @@ import axios from "axios";
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await axios.post("/api/users/signup", {
-      email,
-      password,
-    });
-
-    console.log(response.data);
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+    } catch (error) {
+      setErrors(error.response.data.errors);
+    }
   };
 
   return (
@@ -39,6 +42,17 @@ export default () => {
           className="form-control"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops</h4>
+          <ul className="my-0">
+            {errors.map((error) => (
+              <li key={error.message}>error.message</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
       <button className="btn btn-primary">Sign up</button>
     </form>
   );
