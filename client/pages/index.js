f1a436ce5bdb6116c085b40fbd7cde69d83527b2
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-10-01 01:32:08 -0500

    Get initial props first implementation

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,22 @@
-export default () => {
-  return <h1>Hello from landing</h1>;
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  <h1>Hello from landing</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  let response;
+
+  if (typeof window === "undefined") {
+    // we are in server
+    // request should be performed to nginx-ingress ...
+    response = await axios.get("api/users/current-user");
+  } else {
+    // we are in browser
+  }
+  response = await axios.get("api/users/current-user");
+
+  return response.data;
+};
+
+export default LandingPage;
