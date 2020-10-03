9e220d6d1d542e5e2e033cfac855a183b4c3476c
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-10-03 01:06:24 -0500

    helper function for request added

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,28 +1,15 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>Hello from landing</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    // we are on the server
-    // request should be performed to nginx-ingress ...
-    const { data } = await axios.get(
-      // Remember SERVICE_NAME.NAMESPACE.svc.cluster.local
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current-user",
-      {
-        headers: req.headers,
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const client = await buildClient(context);
+  const { data } = client.get("/api/users/current-user");
 
-    return data;
-  } else {
-    // we are in browser
-    const { data } = await axios.get("api/users/current-user");
-    return data;
-  }
+  return data;
 };
 
 export default LandingPage;
