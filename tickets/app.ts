31c26b86821ca8e49bdb7443af81156b2b96084a
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-10-11 00:50:01 -0500

    setup improved

--- tickets/app.ts
+++ tickets/app.ts
@@ -1,9 +1,28 @@
 import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@gb-tickets/common";
 
 const app = express();
+
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
 
 app.get("/api/tickets/health-check", (req, res) => {
   return res.send({ status: "ok" });
 });
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 export { app };
--- tickets/index.ts
+++ tickets/index.ts
@@ -1,8 +1,24 @@
+import "express-async-errors";
 import { app } from "./app";
+import mongoose from "mongoose";
 
 const PORT = 3000;
 
-const start = () => {
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
   app.listen(PORT, () => {
     console.log(`Listening Tickets on port ${PORT}`);
   });
