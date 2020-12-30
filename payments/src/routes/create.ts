3a0dd864f9b4695cf740855f9ce63a53d1d7323e
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-12-29 23:22:28 -0500

    stripe setup

--- payments/src/routes/create.ts
+++ payments/src/routes/create.ts
@@ -8,6 +8,7 @@ import {
   NotAuthorizedError,
   OrderStatus,
 } from '@gb-tickets/common';
+import { stripe } from '../stripe';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -33,6 +34,14 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100, // convert to cents
+      // source: token,
+      source: 'tok_visa', // testing value
+      description: 'My first charge',
+    });
 
     res.send({ success: true });
   }
