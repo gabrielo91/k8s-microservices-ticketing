2868f20c1213bc0c574ea0825b794b5936768e10
Gabriel Ortega <gabrielo_9103@hotmail.com>
2021-01-03 00:32:39 -0500

    form implemented

--- client/pages/tickets/create.js
+++ client/pages/tickets/create.js
@@ -1,17 +1,24 @@
+import { useForm } from 'react-hook-form';
+
 function NewTicket() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
       <h1>Create a new ticket</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group">
           <label>Title</label>
-          <input className="form-control" />
+          <input className="form-control" name="title" ref={register} />
         </div>
         <div className="form-group">
           <label>Price</label>
-          <input className="form-control" />
+          <input className="form-control" name="price" ref={register} />
         </div>
-        <button className="btn btn-primary">Submit</button>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
