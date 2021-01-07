25b13fcf79a1404d673c21a43dc08dc0b400ff16
Gabriel Ortega <gabrielo_9103@hotmail.com>
2021-01-07 01:03:17 -0500

    ingress bug-fix

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 function buildClient({ req }) {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // baseURL:
+      //   'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://159.89.222.146',
       headers: req.headers,
     });
   } else {
